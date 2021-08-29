69b208950d6fb25b54dd5394d903f7dbc8cd48ae
rahul-017 <rahulitdata@gmail.com>
2021-08-29 22:51:59 +0530

    first commit

---
+++ sales_purchase/sales_purchase/doctype/customer_details/customer_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, RAHUL.S and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('CUSTOMER DETAILS', {
+	// refresh: function(frm) {
+
+	// }
+});
